152c2296dfe3d79cff3f2bf064c85ea9aa92f8f1
Saber FRAJ <frejsaber@gmail.com>
2020-11-15 14:40:43 +0100

    routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router'; 
+import { ChartComponent } from './chart/chart.component';
+const routes: Routes = [
+  {  path: '',  redirectTo: 'first-component',  pathMatch: 'full'},
 
-const routes: Routes = [];
+  { path: 'first-component', component: ChartComponent },
+  { path: 'second-component', component: ChartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
